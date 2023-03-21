cda7f846f063c09cbcd5c24c7e648a86ee35166b
zhaoyujian <603245498@qq.com>
2023-03-21 15:55:00 +0800

    finish 04

--- src/00004-easy-pick/template.ts
+++ src/00004-easy-pick/template.ts
@@ -1,1 +1,7 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+    [P in K]: T[P]
+}
+
+// 题目要求：K从T中pick出字段
+// 解题思路：类似于写一个遍历函数，遍历T，（每个字段属性为P）if判断是否是K的
+// 同时需要约束K中的字段都在T的范围内(extends keyof)
