2fa3ba5649c4d61efacb62ec956f738bc50de47e
zhaoyujian <603245498@qq.com>
2023-03-29 09:41:55 +0800

    format

--- src/00116-medium-replace/template.ts
+++ src/00116-medium-replace/template.ts
@@ -2,7 +2,11 @@ type Replace<
   S extends string,
   From extends string,
   To extends string
-> = From extends '' ? S : S extends `${infer First}${From}${infer Rest}` ? `${First}${To}${Rest}` : S
+> = From extends ""
+  ? S
+  : S extends `${infer First}${From}${infer Rest}`
+  ? `${First}${To}${Rest}`
+  : S;
 
 // 思路比较简单，匹配一下From然后返回成To就行
 // 这样的话case3会报错，那就再加一个对From为空字符串的判断
