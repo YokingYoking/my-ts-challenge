38a9bc0d16deeaf6faa08f5681182e87bbaa570d
zhaoyujian <603245498@qq.com>
2023-04-14 17:09:25 +0800

    finish 3376

---
+++ src/03376-medium-inordertraversal/template.ts
@@ -0,0 +1,15 @@
+interface TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
+type InorderTraversal<T extends TreeNode | null> = [T] extends [TreeNode]
+  ? [...InorderTraversal<T["left"]>, T["val"], ...InorderTraversal<T["right"]>]
+  : [];
+
+  // 本题大意简明，先序遍历树，首先判断是否为空，若为空直接返回空数组
+  // 一开始的想法是判断左节点是否为空，若不是则递归左节点，若是则返回该节点val和对右节点递归
+  // 这样写无法通过是因为没有对T[val]展开，结果不正确，而且实际上左右节点也是写成对称的递归展开即可
+  // 所以直接返回一个数组分别先对左节点递归解构展开，返回该node的value，再对右节点递归解构展开
+  // 这样写test是都通过了，但是汇报一个“类型实例化过深且可能无限”的错误
+  // 这是因为在最前面写T extends TreeNode的时候没有用中括号括起来
